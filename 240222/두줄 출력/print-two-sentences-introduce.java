6ca2207dee06430ebd923595ef72eff44fbadaf0
수지 <118612460+norangsuji@users.noreply.github.com>
2024-02-22 12:23:28 +0900

    Title: 두줄 출력, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240222/두줄 출력/print-two-sentences-introduce.java
+++ 240222/두줄 출력/print-two-sentences-introduce.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        system.out.println("Hello students!");
-        system.out.println("Welcome to LeebrosCode!");
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!");
 
     }
 }
