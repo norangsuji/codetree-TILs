17bb449760fd6868b7413fd174298317bc14aadb
수지 <118612460+norangsuji@users.noreply.github.com>
2024-02-22 12:23:08 +0900

    Title: 두줄 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240222/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        system.out.println("Hello students!");
+        system.out.println("Welcome to LeebrosCode!");
+
+    }
+}
