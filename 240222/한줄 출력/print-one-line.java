3c2407faee268ce94da5afc2a4768d9142eaac95
수지 <118612460+norangsuji@users.noreply.github.com>
2024-02-22 12:21:18 +0900

    Title: 한줄 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240222/한줄 출력/print-one-line.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.print("Let's go LeebrosCode!"); // 3과 5를 출력하는 데 중간에 공백을 넣겠다는 뜻
+    }
+}
